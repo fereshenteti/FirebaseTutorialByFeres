127a9bbb006db42824ae6c67939f34951c5c0200
fereshenteti <feressfax@hotmail.com>
2017-12-05 03:28:25 +0100

    with firebase email auth

--- app/src/main/java/com/henteti/feres/firebasetutorialbyferes/MainActivity.java
+++ app/src/main/java/com/henteti/feres/firebasetutorialbyferes/MainActivity.java
@@ -1,16 +1,114 @@
 package com.henteti.feres.firebasetutorialbyferes;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //region Firebase variables
+    private FirebaseAuth mAuth;
+
+    //endregion
+
+    private AppCompatEditText email, pwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initializing connexion with firebase
+        initFirebase();
+
+        //init view
+        initView();
+
+    }
+
+    private void initView() {
+
+        email = findViewById(R.id.email);
+        pwd = findViewById(R.id.pwd);
+
+
+    }
+
+    private void initFirebase() {
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.create:
+                signUpWithMailandPwd();
+                break;
+            case R.id.connectMail:
+                loginWithMailandPwd();
+                break;
+        }
+    }
 
+    private void loginWithMailandPwd() {
+
+        mAuth.signInWithEmailAndPassword(email.getText().toString(), pwd.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Something went wrong! Please try again.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+    private void signUpWithMailandPwd() {
+
+        mAuth.createUserWithEmailAndPassword(email.getText().toString(), pwd.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //1) register the user in the shared preferences to maintain his connexion
+                            //for next openings of the app
+                            ...
+                            //2) share the user's infos with the singleton so it can be recognized in
+                            //the other activities of the app
+                            ...
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Something went wrong! Please try again.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
 }
